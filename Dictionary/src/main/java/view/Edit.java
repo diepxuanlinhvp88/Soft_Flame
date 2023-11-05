13d8760f62df35fed127695897c3d1c10613a644
diepxuanlinh <22021193@vnu.edu.vn>
2023-11-05 16:28:40 +0700

    add editcontroller

---
+++ Dictionary/src/main/java/view/Edit.java
@@ -0,0 +1,13 @@
+package view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Edit implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
